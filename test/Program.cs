b0931697ebcb4ab05ee1066b217e048c2c9d8426
Ksu000 <ksu.korepanova@mail.ru>
2026-09-14 09:07:54 +0700

    Добавила строчку

--- test/Program.cs
+++ test/Program.cs
@@ -1,2 +1,3 @@
 ﻿Console.WriteLine("Hello, World!");
 Console.WriteLine("Аня писька.");
+Console.WriteLine("Аня крамбл куки");
--- test/obj/Debug/net8.0/test.AssemblyInfo.cs
+++ test/obj/Debug/net8.0/test.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66504a438cafc4c9ca6e2c17dbf7f21078af8dfa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ab627b5748c0c138ffd2810c17b600ebefc39ca")]
 [assembly: System.Reflection.AssemblyProductAttribute("test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
